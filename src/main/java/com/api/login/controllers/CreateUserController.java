86213c15fd679aa29e5746c1df1752b9accf37c8
Luis Chagas <lflchagas@gmail.com>
2022-05-22 08:51:55 -0300

    Archives not used deleted

--- src/main/java/com/api/login/controllers/CreateUserController.java
+++ src/main/java/com/api/login/controllers/CreateUserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("users")
 public class CreateUserController {
     @Autowired
     private UserRepositories repository;
